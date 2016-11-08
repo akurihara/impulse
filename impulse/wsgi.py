b27e040bf2c1ac44e2ea12792a8e39e1452f2424
Alex Kurihara <alex.kurihara@gmail.com>
2016-11-07 23:57:39 -0500

    add Procfile

--- impulse/wsgi.py
+++ impulse/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "impulse.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "impulse.settings.production")
 
 application = get_wsgi_application()
