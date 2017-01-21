9a320f95fb32a6baa107a7018a373c5ad3783985
Alex Kurihara <alex.kurihara@gmail.com>
2017-01-21 01:29:38 -0500

    add static urls

--- impulse/urls.py
+++ impulse/urls.py
@@ -1,4 +1,7 @@
+from django.conf import settings
 from django.conf.urls import url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 from django.contrib import admin
 
 from alert.views import MonitorsHandler
@@ -8,3 +11,5 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^monitors/', MonitorsHandler.as_view(), name='monitors'),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
