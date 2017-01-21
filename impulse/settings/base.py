752ae49644318f5f059269886b35df024b831ba3
Alex Kurihara <alex.kurihara@gmail.com>
2017-01-21 01:08:28 -0500

    change static files configuration

--- impulse/settings/base.py
+++ impulse/settings/base.py
@@ -104,7 +104,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Extra places for collectstatic to find static files.
+# STATICFILES_DIRS = (
+#    os.path.join(BASE_DIR, 'static'),
+# )
+
 
 # django-phonenumber-field
 
