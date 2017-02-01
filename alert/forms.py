1331ede193a1f60216717eff572ff73c864a3c12
Alex Kurihara <alex.kurihara@gmail.com>
2017-02-01 01:06:07 -0500

    remove seatgeek_event_id, event_tile, and datetime_event start columns from Monitor

--- alert/forms.py
+++ alert/forms.py
@@ -8,13 +8,13 @@ class MonitorForm(ModelForm):
 
     class Meta:
         model = Monitor
-        fields = ['amount', 'phone_number', 'seatgeek_event_id']
+        fields = ['amount', 'phone_number', 'event']
 
     def save(form):
         cleaned_data = form.cleaned_data
 
         monitor = monitor_service.create_monitor(
-            seatgeek_event_id=cleaned_data['seatgeek_event_id'],
+            event=cleaned_data['event'],
             phone_number=cleaned_data['phone_number'],
             amount=cleaned_data['amount']
         )
--- alert/models.py
+++ alert/models.py
@@ -4,10 +4,7 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 class Monitor(models.Model):
     amount = models.DecimalField(max_digits=10, decimal_places=2)
-    datetime_event_start = models.DateTimeField()
     phone_number = PhoneNumberField()
-    event_title = models.CharField(max_length=255)
-    seatgeek_event_id = models.CharField(max_length=10)
     event = models.ForeignKey('event.Event', null=True, related_name='monitors')
 
     datetime_created = models.DateTimeField(auto_now_add=True)
