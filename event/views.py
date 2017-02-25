774b85234db041925baf7694eaafb3a40a471ef6
Alex Kurihara <alex.kurihara@gmail.com>
2017-02-24 20:38:07 -0500

    create EventSearchView

--- event/views.py
+++ event/views.py
@@ -1,1 +1,20 @@
-# Create your views here.
+from django.http import HttpResponse
+from django.template import Context, loader
+from django.views import View
+
+from event.services import event_service
+
+
+class EventSearchView(View):
+
+    def get(self, request):
+        query = request.GET.get('q')
+        if query:
+            events = event_service.find_or_create_upcoming_events_matching_query(query)
+        else:
+            events = None
+
+        template = loader.get_template('event/event_search.html')
+        context = Context({'query': query, 'events': events})
+
+        return HttpResponse(template.render(context))
--- impulse/urls.py
+++ impulse/urls.py
@@ -4,12 +4,14 @@ from django.contrib import admin
 from django.views.generic import RedirectView
 
 from alert.views import CreateMonitorView, IncomingSMSMessageView, MonitorDetailView, MonitorsHandler
+from event.views import EventSearchView
 
 
 urlpatterns = [
-    url(r'^$', RedirectView.as_view(pattern_name='monitor-create')),
+    url(r'^$', RedirectView.as_view(pattern_name='events')),
     url(r'^admin/', admin.site.urls),
-    url(r'^incoming-sms-message$', IncomingSMSMessageView.as_view(), name='incoming-sms-message'),
+    url(r'^events/$', EventSearchView.as_view(), name='events'),
+    url(r'^incoming-sms-message/$', IncomingSMSMessageView.as_view(), name='incoming-sms-message'),
     url(r'^monitor/add/$', CreateMonitorView.as_view(), name='monitor-create'),
     url(r'^monitor/(?P<pk>[0-9]+)/$', MonitorDetailView.as_view(), name='monitor-detail'),
     url(r'^monitors/', MonitorsHandler.as_view(), name='monitors'),
