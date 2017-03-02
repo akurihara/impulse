f5c561499ead92303d0129a55eadda9962af80e3
Alex Kurihara <alex.kurihara@gmail.com>
2017-03-02 00:02:01 -0500

    remove trailing slashes from URLs

--- impulse/urls.py
+++ impulse/urls.py
@@ -10,11 +10,11 @@ from event.views import EventDetailView, EventSearchView
 urlpatterns = [
     url(r'^$', RedirectView.as_view(pattern_name='events')),
     url(r'^admin/', admin.site.urls),
-    url(r'^events/$', EventSearchView.as_view(), name='events'),
-    url(r'^events/(?P<event_id>[0-9]+)/$', EventDetailView.as_view(), name='event-detail'),
+    url(r'^events$', EventSearchView.as_view(), name='events'),
+    url(r'^events/(?P<event_id>[0-9]+)$', EventDetailView.as_view(), name='event-detail'),
     url(r'^events/(?P<event_id>[0-9]+)/monitors/(?P<monitor_id>[0-9]+)$', EventDetailView.as_view(), name='event-with-monitor-detail'),
-    url(r'^incoming-sms-message/$', IncomingSMSMessageView.as_view(), name='incoming-sms-message'),
-    url(r'^monitors/$', CreateMonitorView.as_view(), name='create-monitor'),
+    url(r'^incoming-sms-message$', IncomingSMSMessageView.as_view(), name='incoming-sms-message'),
+    url(r'^monitors$', CreateMonitorView.as_view(), name='create-monitor'),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
